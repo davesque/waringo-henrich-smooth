91216f21f9141e307aea74b6bc693ea79bfba010
David Sanders <dsanders@fusionbox.com>
2013-05-01 17:35:48 -0600

    Add first test

--- spec/waringoHenrichSmoothSpec.js
+++ spec/waringoHenrichSmoothSpec.js
@@ -1,7 +1,19 @@
 /*globals describe, it, expect*/
 
 describe("findNeighborhood", function() {
-  it("should exist", function() {
-    expect(true).toBe(true);
+  var COORDS = [
+    {x: 0, y: 0, remove: true},
+    {x: 0, y: 0, remove: true},
+    {x: 0, y: 0, remove: false},
+    {x: 0, y: 0, remove: true},
+    {x: 0, y: 0, remove: false},
+    {x: 0, y: 0, remove: false},
+    {x: 0, y: 0, remove: true},
+  ];
+
+  var findNeighborhood = window.findNeighborhood;
+
+  it("should find the correct neighborhood for a point", function() {
+    expect(findNeighborhood(COORDS, 5)).toEqual([2, 4]);
   });
 });
