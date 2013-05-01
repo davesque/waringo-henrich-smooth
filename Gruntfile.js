f24673f746d986d7e9a010ad0c3206ce0cf8792f
David Sanders <dsanders@fusionbox.com>
2013-05-01 17:34:51 -0600

    Update gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,12 +4,11 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     jasmine: {
-      pivotal: {
-        src: 'src/**/*.js',
-        options: {
-          specs: 'spec/*Spec.js',
-          helpers: 'spec/*Helper.js',
-        }
+      src: 'src/**/*.js',
+      options: {
+        specs: 'spec/*Spec.js',
+        helpers: 'spec/*Helper.js',
+        vendor: 'vendor/**/*.js',
       }
     },
   });
