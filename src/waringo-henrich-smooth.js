8ab6eb0c9bf3ed1b61c98653370f2ec26f19aefb
David Sanders <dsanders@fusionbox.com>
2013-05-02 13:08:45 -0600

    Add pointToLineDistance function

--- src/waringo-henrich-smooth.js
+++ src/waringo-henrich-smooth.js
@@ -19,6 +19,33 @@
  * http://cdn.intechopen.com/pdfs/4283/InTech-Smoothing_of_piecewise_linear_paths.pdf
  */
 
+
+/**
+ * Returns the distance between a point `p2` and the line formed by two points
+ * `p1` and `p3`.
+ */
+function pointToLineDistance(p1, p2, p3) {
+  var d;
+  var a, b, C;
+
+  if ( p1.x === p3.x ) {
+    // Special case for slope == infinity
+    d = p2.x - p1.x;
+  } else if ( p1.y === p3.y ) {
+    // Special case for slope == 0
+    d = p2.y - p1.y;
+  } else {
+    // Normal case
+    a = p3.y - p1.y;
+    b = p3.x - p1.x;
+    C = p1.y;
+    d = Math.abs(a * p2.x + b * p2.y + C) / Math.sqrt(a * a + b * b);
+  }
+
+  return Math.abs(d);
+}
+
+
 /**
  * Finds the indexes of neighboring, non-removed points for the point at the
  * given index in the given coordinate list.
