a4c1dc35a6a3ddcebc11c43affd96843016d2612
David Sanders <dsanders@fusionbox.com>
2013-05-01 23:26:13 -0600

    Add waringoHenrichSmoothSpec.js

---
+++ spec/waringoHenrichSmoothSpec.js
@@ -0,0 +1,10 @@
+/*jshint es5: true*/
+/*globals describe, it, expect*/
+
+describe("waringoHenrichSmooth", function() {
+  var waringoHenrichSmooth = window.waringoHenrichSmooth;
+
+  it("should be true", function() {
+    expect(true).toEqual(true);
+  });
+});
